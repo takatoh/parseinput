258977d3f2061da55b40986ba08ce5086bd1d21a
takatoh <takatoh.m@gmail.com>
2017-11-07 13:08:14 +0900

    main.go: Output raw data.

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ func main() {
 	infile := os.Args[1]
 	input := inputparser.Parse(infile)
 
-	fmt.Printf("GAMMA_R = %v\n", input.Gamma_r)
-	fmt.Printf("H_MAX   = %v\n", input.H_max)
-	fmt.Printf("PLOT    = %v\n", input.Plot)
+//	fmt.Printf("GAMMA_R = %v\n", input.Gamma_r)
+//	fmt.Printf("H_MAX   = %v\n", input.H_max)
+//	fmt.Printf("PLOT    = %v\n", input.Plot)
+	fmt.Printf("%#v\n", input)
 }
