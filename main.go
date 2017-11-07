d3df6402321b4d3c41c11516592ac579700de776
takatoh <takatoh.m@gmail.com>
2017-11-07 12:21:33 +0900

    main.go: Fix output format.

--- main.go
+++ main.go
@@ -12,6 +12,6 @@ func main() {
 	input := inputparser.Parse(infile)
 
 	fmt.Printf("GAMMA_R = %v\n", input.Gamma_r)
-	fmt.Printf("H_MAX =   %v\n", input.H_max)
-	fmt.Printf("PLOT =    %v\n", input.Plot)
+	fmt.Printf("H_MAX   = %v\n", input.H_max)
+	fmt.Printf("PLOT    = %v\n", input.Plot)
 }
