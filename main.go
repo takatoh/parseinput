25d36aec3b82f2e686aa433493b9b46fb7d67340
takatoh <takatoh.m@gmail.com>
2017-11-07 09:10:12 +0900

    main.go: New.

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"./inputparser"
+)
+
+func main() {
+	infile := os.Args[1]
+	input := inputparser.Parse(infile)
+
+	fmt.Printf("GAMMA_R = %v\n", input.Gamma_r)
+	fmt.Printf("H_MAX =   %v\n", input.H_max)
+	fmt.Printf("PLOT =    %v\n", input.Plot)
+}
